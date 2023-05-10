7dca44bdb7d807db04f3a481042a5990e94f13f7
gapima <gapima7@gmail.com>
2023-05-10 01:33:36 -0300

    Criando Base interface do repository

---
+++ Siderum.Infra/Interfaces/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Siderum.Domain.Entities;
+using Siderum.Domain.Entities.Base;
+
+namespace Siderum.Infra.Interfaces;
+
+public interface IBaseRepository<T> where T : EntityBase
+{
+    Task<List<T>> Get();
+    Task<T> Get(Guid id);
+    Task<T> Create(T obj);
+    Task<T> Update(T obj);
+    Task<bool> Delete(Guid id);
+}
